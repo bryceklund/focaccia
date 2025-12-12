51070ebc075b629be0c022216c608f08df2de8eb
bryce <bryce.eklund@gmail.com>
2025-12-11 23:50:52 -0800

    Initial commit (for reals)

---
+++ include/debug.h
@@ -0,0 +1,9 @@
+#ifndef focaccia_debug_h
+#define focaccia_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif //focaccia_debug_h
---
+++ include/value.h
@@ -0,0 +1,18 @@
+#ifndef focaccia_value_h
+#define focaccia_value_h
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+
+#endif //focaccia_value_h
