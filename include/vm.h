6fa8802a8af78d8ff8d459f27adf87892fcd4085
bryce <bryce.eklund@gmail.com>
2025-12-17 21:18:30 -0800

    initial binary operators impl, fatigue

--- include/vm.h
+++ include/vm.h
@@ -2,6 +2,9 @@
 #define focaccia_vm_h
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 typedef struct {
   // Chunk represents a set of bytecode instructions to be executed by the VM
@@ -11,6 +14,9 @@ typedef struct {
   // executed. it's faster to dereference a pointer than it is to look up an
   // index in an array
   uint8_t *ip;
+
+  Value stack[STACK_MAX];
+  Value *stackTop;
 
 } VM; // as in "virtual machine"
 
@@ -23,5 +29,7 @@ typedef enum {
 void initVM();
 void freeVM();
 InterpretResult interpret(Chunk *chunk);
+void push(Value value);
+Value pop();
 
 #endif
